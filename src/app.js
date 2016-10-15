033ab42cbd058531d2fe748c25ae78b8fed029db
nPang <nareerut.pmg@hotmail.com>
2016-10-15 10:56:13 +0700

    add react

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const title ='My React App'
+
+const App =() =>{
+     
+    return <div> My React App </div>
+}
+const elemet =document.getElementById('app')
+ReactDOM.render(<App /> ,elemet)
